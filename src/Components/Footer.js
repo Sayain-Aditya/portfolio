b0f7bff80c6bd79ea434de8405fc602f8423900e
Sayain-Aditya <as4316664@gmail.com>
2024-10-21 18:22:55 +0530

    flex

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,11 @@
-import { FaHome, FaPhone, FaMailBulk, FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
+import {
+  FaHome,
+  FaPhone,
+  FaMailBulk,
+  FaFacebook,
+  FaGithub,
+  FaLinkedin,
+} from "react-icons/fa";
 import "./Footer.css";
 import React from "react";
 
@@ -31,31 +38,32 @@ function Footer() {
               />
               singhaditya83189@gmail.com
             </h4>
-            <p>
-              Hi! I’m Aditya Singh, a fresher passionate about both web and game
-              development. I enjoy building responsive web applications using
-              HTML, CSS, JavaScript, and React. In game development, I bring
-              creative ideas to life through Unity and C#. I’m eager to learn,
-              solve challenges, and work on exciting projects. Let’s connect!
-            </p>
-            <div className="Social">
-            <FaFacebook
-                size={30}
-                style={{ color: "white", marginRight: "1rem" }}
-              />
-              <FaGithub
-                size={30}
-                style={{ color: "white", marginRight: "1rem" }}
-              />
-              <FaLinkedin
-                size={30}
-                style={{ color: "white", marginRight: "1rem" }}
-              />
-            </div>
           </div>
         </div>
-
-        <div className="right"></div>
+        <div className="right">
+          <h4>About Myself</h4>
+          <p>
+            Hi! I’m Aditya Singh, a fresher passionate about both web and game
+            development. I enjoy building responsive web applications using
+            HTML, CSS, JavaScript, and React. In game development, I bring
+            creative ideas to life through Unity and C#. I’m eager to learn,
+            solve challenges, and work on exciting projects. Let’s connect!
+          </p>
+          <div className="Social">
+            <FaFacebook
+              size={30}
+              style={{ color: "white", marginRight: "1rem" }}
+            />
+            <FaGithub
+              size={30}
+              style={{ color: "white", marginRight: "1rem" }}
+            />
+            <FaLinkedin
+              size={30}
+              style={{ color: "white", marginRight: "1rem" }}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
