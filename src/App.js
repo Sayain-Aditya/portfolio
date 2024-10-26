3c08df76c793666b15cfc47d80f02a9886cb60bd
Sayain-Aditya <as4316664@gmail.com>
2024-10-26 23:52:33 +0530

    preloader

--- src/App.js
+++ src/App.js
@@ -4,17 +4,30 @@ import Home from "./Routers/Home"
 import Projects from "./Routers/Projects"
 import About from "./Routers/About"
 import Contact from "./Routers/Contact"
+import Preloader from "./Components/Preloader";
+import  {useEffect, useState} from "react";
 
 import { Route, Routes } from "react-router-dom";
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading time
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // Adjust as needed
+  }, []);
   return (
    <>
+   <Preloader isLoading={isLoading}/>
+   {!isLoading && (
    <Routes>
     <Route path="/" element={<Home />}/>
     <Route path="/project" element={<Projects />}/>
     <Route path="/about" element={<About />}/>
     <Route path="/contact" element={<Contact />}/>
    </Routes>
+   )}
    </>
   );
 }
---
+++ src/Components/Preloader.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Preloader.css';
+
+const Preloader = ({ isLoading }) => {
+  return (
+    <div className={`preloader ${isLoading ? 'fadeIn' : 'fadeOut'}`}>
+      <div className="loader">
+        <svg width="200" height="200">
+          <circle
+            className="traco"
+            cx="100"
+            cy="100"
+            r="70"
+            fill="transparent"
+            stroke="#ffe71c"
+            strokeWidth="9"
+            strokeDasharray="35 20"
+            transform="rotate(31 100 100)"
+          ></circle>
+        </svg>
+        <div className="p1"></div>
+        <div className="p2"></div>
+        <div className="p3"></div>
+        <div className="p4"></div>
+        <div className="p5"></div>
+        <div className="p6"></div>
+        <div className="p7"></div>
+        <div className="p8"></div>
+        <div className="circle">
+          <div className="content">
+            <div className="eyes">
+              <div className="eye-left"></div>
+              <div className="eye-right"></div>
+            </div>
+            <div className="nouse"></div>
+            <div className="mouth"></div>
+            <div className="teeth">
+              <div className="tooth"></div>
+              <div className="tooth"></div>
+              <div className="tooth"></div>
+              <div className="tooth"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Preloader;
