280d0011f5623b4f0a17a355b582a4a1a7fdaedf
Sayain-Aditya <as4316664@gmail.com>
2024-10-27 21:22:05 +0530

    pre

--- src/App.js
+++ src/App.js
@@ -1,35 +1,56 @@
 import React from "react";
-import "./index.css"
-import Home from "./Routers/Home"
-import Projects from "./Routers/Projects"
-import About from "./Routers/About"
-import Contact from "./Routers/Contact"
-import Preloader from "./Components/Preloader";
-import  {useEffect, useState} from "react";
-
+import "./index.css";
+import Home from "./Routers/Home";
+import "./Components/Preloader.css";
+import Projects from "./Routers/Projects";
+import About from "./Routers/About";
+import Contact from "./Routers/Contact";
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
-function App() {
-  const [isLoading, setIsLoading] = useState(true);
+
+const App = () => {
+  const [percentage, setPercentage] = useState(0);
+  const [loadingComplete, setLoadingComplete] = useState(false);
 
   useEffect(() => {
-    // Simulate loading time
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 3000); // Adjust as needed
-  }, []);
+    // const interval = setInterval(() => {
+    //   // setPercentage((prev) => {
+    //   //   if (prev < 100) {
+    //   //     return prev + 1;
+    //   //   }
+    //   // });
+
+    //   // if (percentage < 100) {
+    //   //   setPercentage((prev) => prev + 1);
+    //   // }
+    // }, 50); // Adjust speed by changing the interval duration
+
+    percentage !== 100 &&
+      setTimeout(() => {
+        setPercentage((prev) => prev + 1);
+      }, 45);
+
+    percentage === 100 && setLoadingComplete(true);
+    // percentage === 100 && setPercentage(0); // Stop the interval when loading is complete
+  }, [percentage, loadingComplete]);
+
   return (
-   <>
-   <Preloader isLoading={isLoading}/>
-   {!isLoading && (
-   <Routes>
-    <Route path="/" element={<Home />}/>
-    <Route path="/project" element={<Projects />}/>
-    <Route path="/about" element={<About />}/>
-    <Route path="/contact" element={<Contact />}/>
-   </Routes>
-   )}
-   </>
+    <>
+      {!loadingComplete && (
+        <div className="loading-container">
+          <div className="loading-bar" style={{ width: `${percentage}%` }}>
+            <span className="percentage-text">{percentage}%</span>
+          </div>
+        </div>
+      )}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
